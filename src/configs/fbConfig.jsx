00f8e1a18df19443cb5d9b116f4b381e5ffe47f8
constantine71 <pfshen0627@gmail.com>
2019-11-16 11:35:08 +0800

    Update fbConfig.jsx
    delete config information

--- src/configs/fbConfig.jsx
+++ src/configs/fbConfig.jsx
@@ -4,14 +4,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDJ8flQbigU2jHTuy5PnHy0d5GnjvYhGOw",
-  authDomain: "binggan-plan.firebaseapp.com",
-  databaseURL: "https://binggan-plan.firebaseio.com",
-  projectId: "binggan-plan",
-  storageBucket: "binggan-plan.appspot.com",
-  messagingSenderId: "523718687748",
-  appId: "1:523718687748:web:ac97d75a85cc45d6448951",
-  measurementId: "G-XJEQT1XQLK"
+  //private information
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
